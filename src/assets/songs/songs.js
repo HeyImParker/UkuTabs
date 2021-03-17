d14c86cc3cc79a5698ad635c7e0031de6d4f40c1
HeyImParker <patrickjpark@gmail.com>
2021-03-17 12:21:37 -0600

    added views and needed tab pictures

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,10 @@
+let songs = [{
+    id: 1,
+    title: "Heart of a Girl (transposed)",
+    file: "./HeartOfAGirl.txt"
+},
+{
+    id: 2,
+    title: "Christmas Waltz",
+    file: "./ChristmasWaltz.txt"
+}]
---
+++ src/components/InlineTab.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'InlineTab',
+    props: {
+        word: String,
+        tab: String
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        display: inline;
+    }
+</style>
